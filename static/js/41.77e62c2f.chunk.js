51f661d8ddb4803dfe15e4542754d71d3808cf1c
mmagdy8com <mmagdy@8com.com>
2024-06-26 21:36:39 +0000

    Added my project

---
+++ static/js/41.77e62c2f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[41],{532:function(s,e,t){"use strict";t.r(e);t(0);var a=t(4);e.default=function(s){var e=s.name,t=s.desc;return Object(a.jsxs)("div",{className:"mb-3",children:[Object(a.jsxs)("span",{className:"font-weight-bold text-dark",children:[e,":"]}),Object(a.jsxs)("span",{className:"text-gray-light",children:[" ",t]})]})}}}]);
+//# sourceMappingURL=41.77e62c2f.chunk.js.map
